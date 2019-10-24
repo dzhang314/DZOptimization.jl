cb7ac26b077ce6ac0b16594228116c0f62d8ab71
David K. Zhang <dzhang314@gmail.com>
2019-10-23 17:07:18 -0700

    Add ConstrainedLBFGSOptimizer

--- src/DZOptimization.jl
+++ src/DZOptimization.jl
@@ -2,6 +2,7 @@ module DZOptimization
 
 export StepObjectiveFunctor, ConstrainedStepObjectiveFunctor,
     ConstrainedGradientDescentOptimizer, constrained_gradient_descent_optimizer,
+    ConstrainedLBFGSOptimizer, constrained_lbfgs_optimizer,
     quadratic_line_search, BFGSOptimizer, step!
 
 using LinearAlgebra: mul!
@@ -171,8 +172,9 @@ end
 function step!(opt::ConstrainedGradientDescentOptimizer{S1,S2,S3,T,N}
         ) where {S1,S2,S3,T<:Real,N}
     @inbounds begin
-        x, g, temp = opt.current_point, opt.current_gradient, opt.scratch_space
-        s, y, n = opt.delta_point, opt.delta_gradient, length(x)
+        x, g = opt.current_point, opt.current_gradient
+        s, y = opt.delta_point, opt.delta_gradient
+        n, temp = length(x), opt.scratch_space
         initial_step_size = dot(s, y) / norm2(y)
         if !isfinite(initial_step_size)
             initial_step_size = sqrt(eps(T)) / max(one(T), norm2(g))
@@ -181,23 +183,108 @@ function step!(opt::ConstrainedGradientDescentOptimizer{S1,S2,S3,T,N}
             opt.step_functor, opt.current_objective[1], initial_step_size)
         opt.current_iteration[1] += 1
         opt.current_objective[1] = new_objective
-        @simd ivdep for i = 1 : n
-            temp[i] = x[i] - step_size * g[i]
-        end
+        @simd ivdep for i = 1:n; temp[i] = x[i] - step_size * g[i]; end
         opt.constraint_functor!(temp)
-        @simd ivdep for i = 1 : n
-            s[i] = temp[i] - x[i]
-        end
-        @simd ivdep for i = 1 : n
-            x[i] = temp[i]
-        end
+        @simd ivdep for i = 1:n; s[i] = temp[i] - x[i]            ; end
+        @simd ivdep for i = 1:n; x[i] = temp[i]                   ; end
         opt.gradient_functor!(temp, x)
-        @simd ivdep for i = 1 : n
-            y[i] = temp[i] - g[i]
+        @simd ivdep for i = 1:n; y[i] = temp[i] - g[i]            ; end
+        @simd ivdep for i = 1:n; g[i] = temp[i]                   ; end
+    end
+    return opt
+end
+
+################################################################################
+
+struct ConstrainedLBFGSOptimizer{S1,S2,S3,T<:Real,N}
+    objective_functor::S1
+    gradient_functor!::S2
+    constraint_functor!::S3
+    current_iteration::Vector{Int}
+    current_objective::Vector{T}
+    current_point::Array{T,N}
+    current_gradient::Array{T,N}
+    delta_point::Array{T,N}
+    delta_gradient::Array{T,N}
+    delta_point_history::Matrix{T}
+    delta_gradient_history::Matrix{T}
+    alpha_history::Vector{T}
+    rho_history::Vector{T}
+    history_length::Int
+    step_direction::Array{T,N}
+    scratch_space::Array{T,N}
+    step_functor::ConstrainedStepObjectiveFunctor{S1,S3,T,N}
+end
+
+function constrained_lbfgs_optimizer(
+        objective_functor::S1, gradient_functor!::S2, constraint_functor!::S3,
+        initial_point::AbstractArray{T,N}, m::Int) where {S1,S2,S3,T<:Real,N}
+    current_point = copy(initial_point)
+    constraint_functor!(current_point)
+    current_objective = objective_functor(current_point)
+    current_gradient = similar(current_point)
+    gradient_functor!(current_gradient, current_point)
+    delta_point = zero(current_point)
+    delta_gradient = zero(current_gradient)
+    step_direction = similar(current_point)
+    scratch_space = similar(current_point)
+    step_functor = ConstrainedStepObjectiveFunctor{S1,S3,T,N}(
+        objective_functor, constraint_functor!,
+        current_point, scratch_space, step_direction)
+    return ConstrainedLBFGSOptimizer{S1,S2,S3,T,N}(
+        objective_functor, gradient_functor!, constraint_functor!, Int[0],
+        T[current_objective], current_point, current_gradient,
+        delta_point, delta_gradient,
+        Matrix{T}(undef, length(current_point), m),
+        Matrix{T}(undef, length(current_point), m),
+        Vector{T}(undef, m), Vector{T}(undef, m),
+        m, step_direction, scratch_space, step_functor)
+end
+
+function DZOptimization.step!(opt::ConstrainedLBFGSOptimizer{S1,S2,S3,T,N}
+        ) where {S1,S2,S3,T<:Real,N}
+    @inbounds begin
+        x, g = opt.current_point, opt.current_gradient
+        s, y = opt.delta_point, opt.delta_gradient
+        S, Y = opt.delta_point_history, opt.delta_gradient_history
+        alpha, rho = opt.alpha_history, opt.rho_history
+        m, n, temp = opt.history_length, length(x), opt.scratch_space
+        q, k = opt.step_direction, opt.current_iteration[1] + 1
+        @simd ivdep for j = 1:n; q[j] = g[j]                      ; end
+        for i = k-1 : -1 : max(k-m, 1)
+            c = (i - 1) % m + 1
+            d = zero(T)
+            @simd ivdep for j = 1:n; d += S[j,c] * q[j]           ; end
+            a = alpha[c] = rho[c] * d
+            @simd ivdep for j = 1:n; q[j] -= a * Y[j,c]           ; end
         end
-        @simd ivdep for i = 1 : n
-            g[i] = temp[i]
+        gamma = dot(s, y) / norm2(y)
+        if !isfinite(gamma)
+            gamma = sqrt(eps(T)) / max(one(T), norm2(g))
         end
+        @simd ivdep for j = 1:n; q[j] *= gamma; end
+        for i = max(k-m, 1) : k-1
+            c = (i - 1) % m + 1
+            d = zero(T)
+            @simd ivdep for j = 1:n; d += Y[j,c] * q[j]           ; end
+            beta = alpha[c] - rho[c] * d
+            @simd ivdep for j = 1:n; q[j] += S[j,c] * beta        ; end
+        end
+        step_size, new_objective = quadratic_line_search(
+            opt.step_functor, opt.current_objective[1], one(T))
+        opt.current_iteration[1] = k
+        opt.current_objective[1] = new_objective
+        @simd ivdep for j = 1:n; temp[j] = x[j] - step_size * q[j]; end
+        opt.constraint_functor!(temp)
+        @simd ivdep for j = 1:n; s[j] = temp[j] - x[j]            ; end
+        @simd ivdep for j = 1:n; x[j] = temp[j]                   ; end
+        opt.gradient_functor!(temp, x)
+        @simd ivdep for j = 1:n; y[j] = temp[j] - g[j]            ; end
+        @simd ivdep for j = 1:n; g[j] = temp[j]                   ; end
+        c = (k - 1) % m + 1
+        @simd ivdep for j = 1:n; S[j,c] = s[j]                    ; end
+        @simd ivdep for j = 1:n; Y[j,c] = y[j]                    ; end
+        rho[c] = inv(dot(s, y))
     end
     return opt
 end
