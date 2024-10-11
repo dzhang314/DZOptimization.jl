8f69b4f998c9fdbf82a6c8d100b6e5f0ad15fe92
David K. Zhang <dzhang314@gmail.com>
2024-10-11 11:24:35 -0700

    reformat PCG.jl

--- src/PCG.jl
+++ src/PCG.jl
@@ -1,28 +1,36 @@
 module PCG
 
+
 export random_fill!, random_array
 
-@inline pcg_advance(state::UInt64) =
+
+@inline _pcg_advance(state::UInt64) =
     0x5851F42D4C957F2D * state + 0x14057B7EF767814F
 
-@inline pcg_extract(state::UInt64) = bitrotate(
+
+@inline _pcg_extract(state::UInt64) = bitrotate(
     (xor(state >> 18, state) >> 27) % UInt32, -(state >> 59))
 
-@inline function random_fill!(
-    x::AbstractArray{T,D}, seed::I
-) where {T,D,I<:Integer}
-    state = pcg_advance(0x14057B7EF767814F + (seed % UInt64))
+
+@inline function random_fill!(x::AbstractArray, seed::Integer)
+    state = _pcg_advance(0x14057B7EF767814F + (seed % UInt64))
     for i in eachindex(x)
-        @inbounds x[i] = 2.3283064365386962890625E-10 * pcg_extract(state)
-        state = pcg_advance(state)
+        @inbounds x[i] = 2.3283064365386962890625E-10 * _pcg_extract(state)
+        state = _pcg_advance(state)
     end
     return x
 end
 
-@inline random_array(seed::I, ::Type{T}, dims::NTuple{D,Int}
-) where {T,D,I<:Integer} = random_fill!(Array{T,D}(undef, dims), seed % UInt64)
 
-@inline random_array(seed::I, ::Type{T}, dims::Int...) where {T,I<:Integer} =
+@inline random_array(
+    seed::Integer,
+    ::Type{T},
+    dims::NTuple{D,Int},
+) where {T,D} = random_fill!(Array{T,D}(undef, dims), seed % UInt64)
+
+
+@inline random_array(seed::Integer, ::Type{T}, dims::Int...) where {T} =
     random_array(seed, T, dims)
+
 
 end # module PCG
